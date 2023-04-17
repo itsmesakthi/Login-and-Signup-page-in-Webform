039ebb5687091ebceeb8fdfd1cfcb2a06673010b
itsmesakthi <129639908+itsmesakthi@users.noreply.github.com>
2023-04-17 11:36:48 +0530

    Add files via upload

---
+++ Login.aspx
@@ -0,0 +1,69 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="Home.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="Content/bootstrap.css" rel="stylesheet" />
+    <script src="Scripts/bootstrap.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />
+
+<!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <title>Login and signup</title>
+    <style>
+        body {            
+            background-image: url(bg3.jpg);
+            background-size:cover;
+            background-repeat: no-repeat;
+            font-family: 'Times New Roman', Times, serif;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container">
+            <div class="col-sm-4" style="padding-left: 0px; padding-right: 0px;">
+            </div>
+            <div class="col-sm-4" style="padding-left: 0px; padding-right: 0px;backdrop-filter: blur(10px);background-color:transparent;">
+                <div> 
+                    <h1 style="text-align: center; margin-top: 20px; margin-bottom: 20px;">Login Page</h1>
+                </div>
+                <div style="margin-bottom: 10px;">
+                    <asp:Label runat="server">Username / User Id</asp:Label>
+                    <asp:TextBox runat="server" ID="TextUser" CssClass="form-control" placeholder="Enter the Username"></asp:TextBox>
+                </div>
+                <div style="margin-bottom: 10px;">
+                    <asp:Label runat="server">Password</asp:Label>
+                    <asp:TextBox runat="server" ID="TextPass" TextMode="Password" CssClass="form-control" placeholder="Enter the Password"></asp:TextBox>
+                </div>
+                <div style="text-align: center; margin-bottom: 10px;">
+                    <asp:Button ID="ButtonLogin" runat="server" CssClass="btn-primary" Text="Login" OnClick="ButtonLogin_Click" />
+                    <asp:Button ID="ButtonSignup" runat="server" CssClass="btn-info" Text="Signup" OnClick="ButtonSignup_Click" />
+                    <asp:Button ID="ButtonForget" runat="server" CssClass="btn-danger" Text="Forget password" OnClick="ButtonForget_Click"/>
+                    <asp:Button ID="ButtonClear" runat="server" CssClass="btn-warning" Text="Clear" OnClick="ButtonClear_Click" />
+                </div>
+                <div style="background-color: yellow; color: red;">
+                    <asp:Label runat="server" ID="LogInfo"></asp:Label>
+                </div>
+            </div>
+            <div class="col-sm-4" style="padding-left: 0px; padding-right: 0px;"></div>
+        </div>
+    </form>
+</body>
+</html>
+
+<script>
+    //$(document).ready(function () {
+    //    //alert("hello");
+    //    $("#ButtonLogin").click(function () {
+    //        alert("hello")
+    //    });
+
+    //});
+</script>
---
+++ Login.aspx.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System.Web.Security;
+using System.Web.Script.Services;
+
+namespace Home
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        SqlConnection sqlcon = new SqlConnection(ConfigurationManager.ConnectionStrings["dbcon"].ToString());
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string userid = TextUser.Text;
+                string password = TextPass.Text;
+                sqlcon.Open();
+                string sqlqry = "select * from Login where user_id='" + userid + "' and password='" + password + "'";
+                SqlCommand cmd = new SqlCommand(sqlqry, sqlcon);
+                SqlDataReader sdr = cmd.ExecuteReader();
+                if (sdr.Read())
+                {
+                    LogInfo.Text = "Login Sucessfully......!!";
+                }
+                else
+                {
+                    LogInfo.Text = "UserId & Password Is not correct Try again..!!";
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+            finally
+            {
+                sqlcon.Close();
+            }
+        }          
+
+        public RegNewUserResult DoRegNewUser(String userid, String password)
+        {
+
+            if (this.IsUsernameExists(userid))
+            {
+                return RegNewUserResult.UsernameAlreadyExists;
+            }
+            else if (userid == "" && password == "")
+            {
+                return RegNewUserResult.UsernameorPasswordIsEmpty;
+            } 
+            else if(userid != "" && password != ""){
+                sqlcon.Open();
+                string sqlqry = "INSERT INTO Login (user_id, password) VALUES ('" + userid + "','" + password + "')";
+                SqlCommand sqlcmd = new SqlCommand(sqlqry, sqlcon);
+                sqlcmd.ExecuteNonQuery();
+                TextUser.Text = "";
+                TextPass.Text = "";
+            }
+            sqlcon.Close();
+            return RegNewUserResult.Success;
+        }
+        public Boolean IsUsernameExists(String userid)
+        {
+            string sqlqry = "select user_id from Login where user_id=@user_id";
+            SqlCommand sqlcmd = new SqlCommand(sqlqry, sqlcon);
+            SqlParameter parausername = sqlcmd.Parameters.Add("@user_id", SqlDbType.NVarChar);
+            parausername.Value = userid;
+            try
+            {
+                sqlcon.Open();
+                Object result = sqlcmd.ExecuteScalar();
+                return result != null;
+            }
+            finally
+            {
+                sqlcon.Close();
+            }
+        }
+
+        protected void ButtonClear_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Login.aspx");
+        }
+        protected void ButtonSignup_Click(object sender, EventArgs e)
+        {
+            if (!this.IsValid)
+            {
+                return;
+            }
+            String userid = TextUser.Text;
+            String password = TextPass.Text;
+            RegNewUserResult result = this.DoRegNewUser(userid, password);
+
+            if (result == RegNewUserResult.UsernameAlreadyExists)
+            {
+                LogInfo.Text = ("Username [" + userid + "] is already exists. !");
+            }
+            else if (result == RegNewUserResult.UsernameorPasswordIsEmpty)
+            {
+                LogInfo.Text = ("Username or password should not be empty!!!");
+            }
+            else if (result == RegNewUserResult.DatabaseFail)
+            {
+                //JSHelper.MsgBox("Database error, please try later.");
+                LogInfo.Text = ("Database error, please try later.");
+            }
+            else if (result == RegNewUserResult.Success)
+            {
+                FormsAuthentication.SetAuthCookie(userid, false);
+                LogInfo.Text = ("Signup Completed");
+                //Response.Redirect("~/Login.aspx");
+            }
+        }
+
+        public ForgetPasswordResult DoUpdateForgetPassword(string userid, string password)
+        {
+            if (userid == "" && password == "")
+            {
+                return ForgetPasswordResult.UsernameorPasswordIsEmpty;
+            }
+            try
+            {
+                sqlcon.Open();
+                string sqlqry = "UPDATE Login SET password='" + password + "' WHERE user_id ='" + userid + "'";
+                SqlCommand sqlcmd = new SqlCommand(sqlqry, sqlcon);
+                sqlcmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            sqlcon.Close();
+            return ForgetPasswordResult.success;
+
+        }
+        protected void ButtonForget_Click(object sender, EventArgs e)
+        {
+            if (!this.IsValid)
+            {
+                return;
+            }
+            string userid = TextUser.Text;
+            string password = TextPass.Text;
+            ForgetPasswordResult result = this.DoUpdateForgetPassword(userid, password);
+
+            if (result == ForgetPasswordResult.UsernameorPasswordIsEmpty)
+            {
+                LogInfo.Text = ("Username and password should not be empty!!!");
+            }
+            else if (result == ForgetPasswordResult.DatabaseFail)
+            {
+                //JSHelper.MsgBox("Database error, please try later.");
+                LogInfo.Text = ("Database error, please try later.");
+            }
+            else if (result == ForgetPasswordResult.success)
+            {
+                LogInfo.Text = ("Password update completed successfully.");
+                //Response.Redirect(url: "~/Login.aspx");
+                //Response.Redirect("~/Login.aspx");
+            }
+
+        }
+        public enum RegNewUserResult
+        {
+            Success,
+            UsernameAlreadyExists,
+            UsernameorPasswordIsEmpty,
+            DatabaseFail
+        }
+        public enum ForgetPasswordResult
+        {
+            success,
+            UsernameorPasswordIsEmpty,
+            DatabaseFail
+        }
+    }
+}
